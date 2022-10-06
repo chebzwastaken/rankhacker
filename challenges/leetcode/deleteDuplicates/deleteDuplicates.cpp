b3e8c792ff6cf939fd072d47edf815d17985fef2
Jesse Doka <jgodwin1420@gmail.com>
2022-10-06 16:02:18 +0100

    deleteDuplicates problem

---
+++ challenges/leetcode/deleteDuplicates/deleteDuplicates.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL) return NULL;
+        ListNode* p = head;
+        while (p->next != NULL) {
+            if (p->val == p->next->val) {
+                p->next = p->next->next;
+            } else {
+                p = p->next;
+            }
+        }
+        return head;
+    }
+};
+
+
+    
