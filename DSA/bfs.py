216845073b0f5538fd7de229a1770da893f6bb0e
jessedoka <jdoka42@gmail.com>
2024-07-01 15:43:46 +0100

    Implement breadth-first search algorithm in Python

---
+++ DSA/bfs.py
@@ -0,0 +1,9 @@
+def BFS(graph, start):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
