2f78f08ed40aadaf2eacb31b397e2002ea5432f5
jessedoka <jdoka42@gmail.com>
2024-07-01 15:44:33 +0100

    refactor: Remove unused code in math/euclid.py

--- math/euclid.py
+++ math/euclid.py
@@ -1,9 +1,7 @@
 def euclid(a, b):
     """Return the greatest common divisor of a and b."""
-    v = []
     if b == 0:
         return a
-    v.append([a // b, a % b])
     # a // b -> whole number
     # a % b -> remainder
     return euclid(b, a % b)
