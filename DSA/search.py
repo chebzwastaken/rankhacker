f85bf692e16d1b4269f367ff1fa7e321f1917e97
jessedoka <jdoka42@gmail.com>
2024-07-23 13:52:27 +0100

    refactor: moved algos into there own functions

--- DSA/search.py
+++ DSA/search.py
@@ -10,7 +10,30 @@ def binarySearch(arr, l, r, x):
     else:
         return -1
     
+def BFS(graph, start):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+    return visited
+
+def DFS(graph, start):
+    visited = set()
+    stack = [start]
+    while stack:
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+
 
 
 arr = [1,2,3,4,5]
-print(arr[7 % len(arr)]) 
+print(arr[7 % len(arr)])  
+
+binarySearch([1,2,3,4,5], 0, len(arr), 3)
--- DSA/sort.py
+++ DSA/sort.py
@@ -30,8 +30,49 @@ def quickSort(arr, low, high):
         quickSort(arr, low, pi-1)
         quickSort(arr, pi+1, high)
 
+
+# mergesort
+
+def mergeSort(arr):
+    if len(arr) > 1:
+ 
+        mid = len(arr)//2 # Finding the mid of the array
+        L = arr[:mid] # Dividing the array elements
+        R = arr[mid:] # into 2 halves
+ 
+        mergeSort(L) # Sorting the first half
+        mergeSort(R) # Sorting the second half
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+
 # Driver code to test above
 arr = [10, 7, 8, 9, 1, 5]
 n = len(arr)
 quickSort(arr, 0, n-1)
-print ("Sorted array is:", arr)
+print("Quick Sort Sorted array is:", arr)
+
+arr = [12, 11, 13, 5, 6, 7]
+mergeSort(arr)
+print("Merge Sort Sorted array is:", arr)
