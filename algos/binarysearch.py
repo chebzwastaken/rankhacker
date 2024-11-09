013dc5fa9ca4e4693cc55c897f20460b0b77c89a
jessedoka <jdoka42@gmail.com>
2024-11-09 18:15:36 +0000

    simple  binary search

---
+++ algos/binarysearch.py
@@ -0,0 +1,17 @@
+def binarySearch(arr, target):
+
+    low = 0 
+    high = len(arr) - 1 
+
+    while low <= high: 
+        mid = (low + high) // 2 
+        if arr[mid] > target:
+            high = mid - 1
+            print(low, high, mid)
+        elif arr[mid] < target:
+            low = mid + 1
+            print(low, high, mid)
+        else:
+            return f"It's Here {arr[mid]}"
+    return False
+print(binarySearch([1, 2, 3, 4, 5], 1))
